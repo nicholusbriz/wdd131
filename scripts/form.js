371da2e4f3a00fb740a767f39b4c48272e241873
nicholusbriz <atbriz256@gmail.com>
2025-10-01 18:25:35 +0300

    form

--- scripts/form.js
+++ scripts/form.js
@@ -43,3 +43,12 @@ document.addEventListener('DOMContentLoaded', function() {
     productSelect.appendChild(option);
   });
 });
+
+const currentYear = document.getElementById('year');
+const lastModified = document.getElementById('lastModified');
+
+const year = new Date().getFullYear();
+currentYear.textContent = year;
+
+const lastMod = new Date(document.lastModified);
+lastModified.textContent = `Last Updated: ${lastMod.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}`;
